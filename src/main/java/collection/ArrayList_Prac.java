2a965b70ee3ecc068b03ac16a368810c93f4b4dd
sasca37 <sasca37@naver.com>
2021-04-20 21:12:16 +0900

    aa

---
+++ src/main/java/collection/ArrayList_Prac.java
@@ -0,0 +1,24 @@
+package collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayList_Prac {
+
+	public static void main(String[] args) {
+		List<String> arr = new ArrayList();
+		arr.add("a");
+		arr.add("a");
+		arr.add("a");
+		arr.add("a");
+		arr.add("a");
+		arr.add("a");
+		arr.add("a");
+		arr.add("a");
+		
+		for(String t : arr) {
+			System.out.println(t);
+		}
+	}
+
+}
---
+++ src/main/java/collection/컬렉션설명.java
@@ -0,0 +1,15 @@
+package collection;
+
+
+public class 컬렉션설명 {
+	/*
+	 컬렉션 프레임워크 - 인터페이스 
+	 1. Collection 
+	 	List: 순서가 있고, 중복 허용 (ArrayList, LinkedList, Stack, Iterator)
+	 	Set 순서, 중복 없음 ( HashSet, SortedSet, TreeSet) 
+	 2. Map 
+	 Hash : 값이 중복될 수 없음 
+	 Tree : 이진 트리 순회 방법 중 중위 순회 a 
+	 */
+
+}
